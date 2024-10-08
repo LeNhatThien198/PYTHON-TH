312d828322e1d9d5bb7d1a88dd52d87ed49a9370
Lê Nhật Thiên <lenhatthien198@gmail.com>
2024-10-08 15:22:53 +0700

    DTH225771.Chuong03.Bai01.py

--- Python TH C3/chuong3/DTH225771.Chuong03.Bai01.py
+++ Python TH C3/chuong3/DTH225771.Chuong03.Bai01.py
@@ -1,1 +1,11 @@
+print("Chương trình kiểm tra năm nhuần")
 
+year=int(input("Mời bạn nhập vào 1 năm: \n"))
+
+if(year % 4 ==0 and year % 100 !=0) or year %400 ==0:
+
+  print("Năm", year, "là năm nhuần")
+
+else:
+
+    print("Năm", year, "không nhuần")
